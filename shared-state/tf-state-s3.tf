36c5175fe32a5905713c2fc831ffec7e43fcc656
Matheus Carino <matheuscarino@gmail.com>
2023-07-01 20:55:59 -0300

    enforcing bucket_acl and bucket_versioning

--- shared-state/tf-state-s3.tf
+++ shared-state/tf-state-s3.tf
@@ -1,29 +1,63 @@
 resource "aws_s3_bucket" "clusters_tf_state_s3_bucket" {
   bucket = "${var.clusters_name_prefix}-terraform-state"
-  acl    = "private"
-  versioning {
-    enabled = false
-  }
   lifecycle {
     prevent_destroy = false
   }
   tags = {
-    Name      = "${var.clusters_name_prefix} S3 Remote Terraform State Store"
+    Name      = "${var.clusters_name_prefix} S3 bucket to store remote Terraform State"
     ManagedBy = "terraform"
   }
 }
 
+resource "aws_s3_bucket_ownership_controls" "clusters_tf_state_s3_bucket_owner" {
+  bucket = aws_s3_bucket.clusters_tf_state_s3_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "clusters_tf_state_s3_bucket_acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.clusters_tf_state_s3_bucket_owner]
+  bucket = aws_s3_bucket.clusters_tf_state_s3_bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "clusters_tf_state_s3_bucket_versioning" {
+  bucket = aws_s3_bucket.clusters_tf_state_s3_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+#######################
+
 resource "aws_s3_bucket" "clusters_vpc_tf_state_s3_bucket" {
   bucket = "${var.clusters_name_prefix}-vpc-terraform-state"
-  acl    = "private"
-  versioning {
-    enabled = false
-  }
   lifecycle {
     prevent_destroy = false
   }
   tags = {
-    Name      = "${var.clusters_name_prefix} VPC S3 Remote Terraform State Store"
+    Name      = "${var.clusters_name_prefix} S3 bucket to store remote Terraform State"
     ManagedBy = "terraform"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "clusters_vpc_tf_state_s3_bucket_owner" {
+  bucket = aws_s3_bucket.clusters_vpc_tf_state_s3_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "clusters_vpc_tf_state_s3_bucket_acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.clusters_vpc_tf_state_s3_bucket_owner]
+  bucket = aws_s3_bucket.clusters_tf_state_s3_bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "clusters_vpc_tf_state_s3_bucket_versioning" {
+  bucket = aws_s3_bucket.clusters_vpc_tf_state_s3_bucket.id
+  versioning_configuration {
+    status = "Enabled"
   }
 }
